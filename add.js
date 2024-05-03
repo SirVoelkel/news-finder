33829ebf6c9637078d83edf6382b3369e0c15c89
Logan_Völkel <Logan_you_dont_know@Fuckoff.com>
2024-05-03 08:34:52 +0200

    news-finder-1.0.0

--- add.js
+++ add.js
@@ -21,14 +21,14 @@ function fetchNews(isSearching){
         url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}&page{currentPage}`;
     }
 
-    fetch(url).then(response => response.json()).then(data => {
+    fetch(url).then(response => response.json()).then(data =>{
         const newsContainer = document.getElementById('newsContainer');
 
         if (currentPage === 1) {
             newsContainer.innerHTML = '';
         }
 
-        const articlesWithImage = data.aticles.filter(article => article.urlToImage);
+        const articlesWithImage = data.articles.filter(article => article.urlToImage);
 
         if (articlesWithImage === 0 || articlesWithImage.length === lastArticleCount) {
             displayNoMoreNews();
@@ -37,21 +37,21 @@ function fetchNews(isSearching){
 
         lastArticleCount = articlesWithImage.length;
 
-        articlesWithImage.foreach(article =>{
+        articlesWithImage.forEach(article => {
             const newsItem = `
-                <div className="newsItem">
-                    <div className="newsImage">
+                <div class="newsItem">
+                    <div class="newsImage">
                         <img src="${article.urlToImage}" alt="${article.title}">
                     </div>
-                    <div className="newsContent">
-                        <div className="info">
+                    <div class="newsContent">
+                        <div class="info">
                             <h5>${article.source.name}</h5>
                             <span> | </span>
                             <h5>${article.puplishedAt}</h5>
                         </div>
                         <h2>${article.title}</h2>
                         <p>${article.description}</p>
-                        <a href="${article.url} target="_blank">Lese mehr<a/>
+                        <a href="${article.url} target="_blank">mehr Lesen<a/>
                     </div>
                 </div>    
             `;
@@ -66,3 +66,44 @@ function fetchNews(isSearching){
         isLoading = false;
     });    
 }    
+
+function displayNoMoreNews() {
+    const newsContainer = document.getElementById('newsContainer');
+    newsContainer.innerHTML += '<p>Keine weiteren Nachrichten laden</p>'; 
+}
+
+window.onscroll = function() {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 10) {
+        if (currentKeyword) {
+            fetchNews(true);
+        }
+        else{
+            fetchNews(false);
+        }
+    }
+}
+
+document.getElementById('searchKeyword').addEventListener('input', function(){
+    currentPage = 1;
+    currentCategory = null;
+    currentKeyword = this.value; 
+});
+
+document.getElementById('fetchCategory').addEventListener('click', function(){
+    currentPage = 1;
+    currentKeyword = null;
+    fetchNews(false);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
